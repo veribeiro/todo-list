b70847b181e2a4711c445aef0000f096eb2a79df
Verônica <veronica.rib16@gmail.com>
2026-01-09 22:04:23 -0300

    Quinto commit - Criando outra ligação de caminhos entre pastas

--- assets/js/songs.js
+++ assets/js/songs.js
@@ -1,20 +1,20 @@
 //Array das músicas
 export default [
     {
-        src: "../assets/musics/lofi-study-calm.mp3",
+        src: "assets/musics/lofi-study-calm.mp3",
         name: "Lofi - FASSounds",
-        img: "../assets/img/fassounds.png"
+        img: "assets/img/fassounds.png"
     },
 
     {
-        src: "../assets/musics/lofi-vividillustrate.mp3",
+        src: "assets/musics/lofi-vividillustrate.mp3",
         name: "Lofi - Vividillustrate",
-        img: "../assets/img/vividillustrate.png"
+        img: "assets/img/vividillustrate.png"
     },
 
     {
-        src: "../assets/musics/lofi-girl-lofi-ambient.mp3",
+        src: "assets/musics/lofi-girl-lofi-ambient.mp3",
         name: "Lofi - lofidreams",
-        img: "../assets/img/lofi-girl.png"
+        img: "assets/img/lofi-girl.png"
     }
 ];
